7d05c5cb83abf23ed13a49cbdb2b82f1c373b3e9
Dennis Pendley <dpend01@jaguar.tamu.edu>
2020-06-17 21:51:50 -0500

    dpend01

---
+++ assets/js/main.js
@@ -0,0 +1,50 @@
+(function($) {
+
+	"use strict";
+
+	var fullHeight = function() {
+
+		$('.js-fullheight').css('height', $(window).height());
+		$(window).resize(function(){
+			$('.js-fullheight').css('height', $(window).height());
+		});
+
+	};
+	fullHeight();
+
+	$('#sidebarCollapse').on('click', function () {
+      $('#sidebar').toggleClass('active');
+  });
+
+       $('#myCard').on('click',function(){
+  $('.card').css({
+      "z-index": "9999",
+      "width": "100%",
+      "height": "100%", 
+      "position": "absolute",
+      "top": "0",
+      "left": "0"});
+    });
+  
+    $(document).ready(function() {
+        $(".menu-icon").on("click", function() {
+              $("nav ul").toggleClass("showing");
+        });
+    });
+    
+    // Scrolling Effect
+    
+    $(window).on("scroll", function() {
+        if($(window).scrollTop()) {
+              $('nav').addClass('koamaru');
+        }
+    
+        else {
+              $('nav').removeClass('koamaru');
+        }
+    })  
+
+    
+})(jQuery);
+
+
